aa95362f2b51ea55730ddb73d9a2d83c06ffba4f
HirokiShinoda <HirokiShinoda@192.168.1.14>
2018-05-27 16:45:50 +0900

    [update]draw method is update.if oneself is scrap,draw yellow rectangle.

--- src/characters/Enemy.java
+++ src/characters/Enemy.java
@@ -3,26 +3,49 @@ package characters;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Enemy extends Character{
-	private final int cell_size = 32;
+import common.Screen;
+
+public class Enemy extends Character implements Screen{
 	private boolean scrap;
 
+	/*
+	 * Enemyクラスのコンストラクタ
+	 * */
 	public Enemy() {
 		scrap = false;
 	}
 
+	/*
+	 * 敵移動処理
+	 */
 	@Override
 	public void move(int move_x, int move_y) {
-		super.setX(super.getX() + move_x);
-		super.setY(super.getY() + move_y);
+		// 自分がスクラップでなければ移動
+		if(!scrap) {
+			super.setX(super.getX() + move_x);
+			super.setY(super.getY() + move_y);
+		}
 	}
 
+	/*
+	 * 敵描画処理
+	 */
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(Color.black);
-		g.fillOval(super.getX() * cell_size + (cell_size/2)/2,super.getY() * cell_size + (cell_size/2)/2,cell_size/2,cell_size/2);
+		// スクラップなら黄色い四角
+		// スクラップでないなら黒丸
+		if(!scrap) {
+			g.setColor(Color.black);
+			g.fillOval(super.getX() * CELLSIZE + (CELLSIZE/2)/2,super.getY() * CELLSIZE + (CELLSIZE/2)/2,CELLSIZE/2,CELLSIZE/2);
+		}else {
+			g.setColor(Color.yellow);
+			g.fillRect(super.getX() * CELLSIZE + (CELLSIZE/2)/2,super.getY() * CELLSIZE + (CELLSIZE/2)/2,CELLSIZE/2,CELLSIZE/2);
+		}
 	}
 
+	/*
+	 * getter,setterを以下に記述する
+	 * */
 	public boolean isScrap() {
 		return scrap;
 	}
