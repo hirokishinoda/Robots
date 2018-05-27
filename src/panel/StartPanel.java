546834740d4b78c91e3b4ed78e21c59d8a1e0325
HirokiShinoda <HirokiShinoda@192.168.1.14>
2018-05-27 16:49:37 +0900

    [add]title screnn add.

--- src/panel/StartPanel.java
+++ src/panel/StartPanel.java
@@ -1,12 +1,64 @@
 package panel;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
-public class StartPanel extends JPanel{
+import common.Screen;
+import main.MyFrame;
 
-	public StartPanel(JFrame frame) {
 
+public class StartPanel extends JPanel implements KeyListener,Screen{
+	private MyFrame my_frame;
+
+	public StartPanel(MyFrame frame) {
+		this.my_frame = frame;
+
+		my_frame.addKeyListener(this);
+
+		setLayout(null);
+
+		setBackground(Color.black);
+		// パネルサイズの設定
+		setPreferredSize(new Dimension(MY_WIDTH, MY_HEIGHT));
 	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		myDrawString(g,Color.white,"Robots!",TITLEX,TITLEY,75);
+		myDrawString(g,Color.white,"--Press [SPACE]key--",SUBTITLEX,SUBTITLEY,25);
+	}
+
+	/*
+	 * 文字列を指定位置、指定色で描画するメソッド
+	 */
+	private void myDrawString(Graphics g,Color c,String str,int x,int y,int str_size){
+		g.setColor(c);
+    	g.setFont(new Font("Arial",Font.BOLD, str_size));
+    	g.drawString(str, x, y);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			my_frame.removeKeyListener(this);
+			my_frame.setState(GAME);
+			System.out.println(e.getKeyCode()+"MENU");
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
 
 }
