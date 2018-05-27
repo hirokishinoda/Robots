1ddc86082de252d18e0df92fc7cafde2850c381d
HirokiShinoda <HirokiShinoda@192.168.1.14>
2018-05-27 16:52:58 +0900

    [remove] constant remove.
    [add] Screnn interface inheritance.

--- src/characters/Player.java
+++ src/characters/Player.java
@@ -3,18 +3,25 @@ package characters;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Player extends Character{
-	private final int cell_size = 32;
+import common.Screen;
 
+public class Player extends Character implements Screen{
+
+	/*
+	 * Player移動処理
+	 */
 	@Override
 	public void move(int move_x,int move_y) {
 		super.setX(super.getX() + move_x);
 		super.setY(super.getY() + move_y);
 	}
 
+	/*
+	 * プレイヤー描画処理(赤丸)
+	 */
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.red);
-		g.fillOval(super.getX() * cell_size + (cell_size/2)/2,super.getY() * cell_size + (cell_size/2)/2,cell_size/2,cell_size/2);
+		g.fillOval(super.getX() * CELLSIZE + (CELLSIZE/2)/2,super.getY() * CELLSIZE + (CELLSIZE/2)/2,CELLSIZE/2,CELLSIZE/2);
 	}
 }
