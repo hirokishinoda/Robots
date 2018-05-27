071d5f9a5dfd9bc8cd6c62750bc3c0c6212c1e22
HirokiShinoda <HirokiShinoda@192.168.1.14>
2018-05-27 16:53:46 +0900

    [remove] conatracter remove.

--- src/gameparts/CollisionManager.java
+++ src/gameparts/CollisionManager.java
@@ -2,10 +2,9 @@ package gameparts;
 
 public class CollisionManager {
 
-	public CollisionManager() {
-
-	}
-
+	/*
+	 * 敵とプレイヤーの衝突判定を行う
+	 * */
 	public boolean collisionPlayerAndEnemy(Square square) {
 		if(square.isEnemy()) {
 			return true;
@@ -13,6 +12,9 @@ public class CollisionManager {
 		return false;
 	}
 
+	/*
+	 * 敵同士の衝突判定を行う
+	 * */
 	public boolean collisionEnemyAndEnemy(Square square) {
 		if(square.getEnemy_num() > 1) {
 			return true;
